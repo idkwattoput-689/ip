d5722894137eee4ffa05be68907baac4f4881668
Ziyang <ziyanghe8@gmail.com>
2026-08-18 20:56:18 +0800

    Add task status update command

--- src/main/java/Gooble.java
+++ src/main/java/Gooble.java
@@ -23,6 +23,8 @@ public class Gooble {
 
         Scanner scanner = new Scanner(System.in);
         String[] tasks = new String[MAX_TASKS];
+        /** Tracks whether each corresponding task has been marked as done. */
+        boolean[] completed = new boolean[MAX_TASKS];
         int taskCount = 0;
 
         while (scanner.hasNextLine()) {
@@ -36,8 +38,24 @@ public class Gooble {
             }
 
             if (command.equals("list")) {
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < taskCount; i++) {
-                    System.out.println((i + 1) + ". " + tasks[i]);
+                    String status = completed[i] ? "[X]" : "[ ]";
+                    System.out.println((i + 1) + "." + status + " " + tasks[i]);
+                }
+            } else if (command.startsWith("mark ")) {
+                String taskNumber = command.substring("mark ".length()).trim();
+                try {
+                    int taskIndex = Integer.parseInt(taskNumber) - 1;
+                    if (taskIndex < 0 || taskIndex >= taskCount) {
+                        System.out.println("That task number does not exist.");
+                    } else {
+                        completed[taskIndex] = true;
+                        System.out.println("Nice! I've marked this task as done:");
+                        System.out.println("  [X] " + tasks[taskIndex]);
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println("Please provide a valid task number.");
                 }
             } else {
                 tasks[taskCount] = command;
