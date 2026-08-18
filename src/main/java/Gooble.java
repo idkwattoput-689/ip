24ebcd96f22b431aafdeaf869478d21854cdf7b8
Ziyang <ziyanghe8@gmail.com>
2026-08-18 14:32:00 +0800

    Updated code to greet the user

--- src/main/java/Gooble.java
+++ src/main/java/Gooble.java
@@ -1,10 +1,21 @@
+/**
+ * Starts Gooble, displays its greeting, and exits.
+ */
 public class Gooble {
     public static void main(String[] args) {
+        String divider = "_".repeat(60);
         String banner = "  ____            _     _      \n"
                 + " / ___| ___   ___ | |__ | | ___ \n"
                 + "| |  _ / _ \\ / _ \\| '_ \\| |/ _ \\\n"
                 + "| |_| | (_) | (_) | |_) | |  __/\n"
                 + " \\____|\\___/ \\___/|_.__/|_|\\___|\n";
-        System.out.println(banner);
+
+        System.out.println(divider);
+        System.out.print(banner);
+        System.out.println("Hello! I'm Gooble.");
+        System.out.println("What can I do for you?");
+        System.out.println(divider);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(divider);
     }
 }
