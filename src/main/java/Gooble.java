920ce91f8e0bcfced18aec0ad3b17e5aa2524583
Ziyang <ziyanghe8@gmail.com>
2026-08-18 14:53:21 +0800

    Added message echo to bot

--- src/main/java/Gooble.java
+++ src/main/java/Gooble.java
@@ -1,5 +1,7 @@
+import java.util.Scanner;
+
 /**
- * Starts Gooble, displays its greeting, and exits.
+ * Starts Gooble, responds to entered commands, and exits when requested.
  */
 public class Gooble {
     public static void main(String[] args) {
@@ -15,7 +17,20 @@ public class Gooble {
         System.out.println("Hello! I'm Gooble.");
         System.out.println("What can I do for you?");
         System.out.println(divider);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(divider);
+
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            String command = scanner.nextLine();
+            System.out.println(divider);
+
+            if (command.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(divider);
+                break;
+            }
+
+            System.out.println(command);
+            System.out.println(divider);
+        }
     }
 }
