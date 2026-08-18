a2777a0657d3ebe0c957b47b83a5f668d1549a88
Ziyang <ziyanghe8@gmail.com>
2026-08-18 14:29:22 +0800

    Debugged banner output

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Gooble User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Gooble.java
@@ -0,0 +1,10 @@
+public class Gooble {
+    public static void main(String[] args) {
+        String banner = "  ____            _     _      \n"
+                + " / ___| ___   ___ | |__ | | ___ \n"
+                + "| |  _ / _ \\ / _ \\| '_ \\| |/ _ \\\n"
+                + "| |_| | (_) | (_) | |_) | |  __/\n"
+                + " \\____|\\___/ \\___/|_.__/|_|\\___|\n";
+        System.out.println(banner);
+    }
+}
